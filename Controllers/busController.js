e89d4cff3ec3733d4f32748404c57da239292f8e
darlingson <dmakuwila@gmail.com>
2024-03-21 22:58:04 +0200

    modified the busController to use the sqlite db

--- Controllers/busController.js
+++ Controllers/busController.js
@@ -1,10 +1,32 @@
-const Bus = require('../Models/busModel');
+// const Bus = require('../Models/busModel');
+
+// exports.getAllBuses = async (req, res) => {
+//   try {
+//     const buses = await Bus.find();
+//     res.json(buses);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// };
+
+const sqlite3 = require('sqlite3').verbose();
+
+// Connect to SQLite database
+const db = new sqlite3.Database('../mydatabase.db', (err) => {
+  if (err) {
+    console.error('Error connecting to SQLite database:', err.message);
+  } else {
+    console.log('Connected to SQLite database.');
+  }
+});
 
 exports.getAllBuses = async (req, res) => {
-  try {
-    const buses = await Bus.find();
+  const sql = `SELECT * FROM Bus`;
+
+  db.all(sql, [], (err, buses) => {
+    if (err) {
+      return res.status(500).json({ message: err.message });
+    }
     res.json(buses);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+  });
 };
